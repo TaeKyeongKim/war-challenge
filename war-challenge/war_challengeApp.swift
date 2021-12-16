697aee39e6a1ccb24f7be8f90eff927878818d84
TaeKyeongKim <rlaxorud1324@naver.com>
2021-12-16 21:43:11 +0900

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Kim Tae Kyeong  on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
